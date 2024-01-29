e6514ef42c69cf251796334ad484dbb4fbde1359
becksonau <twm0987744783@gmail.com>
2024-01-29 13:38:47 +0800

    create Student

---
+++ src/main/java/com/beckson/school/pojo/student.java
@@ -0,0 +1,105 @@
+package com.beckson.school.pojo;
+
+import java.util.Date;
+
+public class student {
+
+    private Integer id ;
+    private String sid;
+    private String name;
+    private String gender;
+    private String classroom;
+    private Date createDate;
+    private Date updateDate;
+
+    public student() {
+    }
+
+    public student(Integer id, String sid, String name, String gender, String classroom, Date createDate, Date updateDate) {
+        this.id = id;
+        this.sid = sid;
+        this.name = name;
+        this.gender = gender;
+        this.classroom = classroom;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+
+    public student(String sid, String name, String gender, String classroom, Date createDate, Date updateDate) {
+        this.sid = sid;
+        this.name = name;
+        this.gender = gender;
+        this.classroom = classroom;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id=" + id +
+                ", sid='" + sid + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", classroom='" + classroom + '\'' +
+                ", createDate=" + createDate +
+                ", updateDate=" + updateDate +
+                '}';
+    }
+}
