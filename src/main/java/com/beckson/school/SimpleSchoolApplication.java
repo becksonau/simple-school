3b94707a6ecc49b835ef58293c583218ed558a0c
becksonau <twm0987744783@gmail.com>
2024-01-29 13:23:16 +0800

    init project

---
+++ src/main/java/com/beckson/school/SimpleSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.beckson.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beckson/school/SimpleSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beckson.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
